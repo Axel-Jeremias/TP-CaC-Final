02c9ac30d3a0d03c99450d28cc5d0b31a0b373e2
Axel-Jeremias <axeljeremias1@gmail.com>
2023-11-28 14:38:47 -0300

    Eliminar archivos de Vue no usados

--- static/js/faq/contacto.js
+++
@@ -1,8 +0,0 @@
-import * as Vue from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
-import { contactoCore } from "./contactoCore.Vue.js";
-
-const { createApp } = Vue;
-
-const mainApp = createApp(contactoCore);
-
-mainApp.mount("#vue-app");
