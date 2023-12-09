1c79c3183948761aff25898503228c39b91b8995
Axel-Jeremias <axeljeremias1@gmail.com>
2023-12-09 14:22:38 -0300

    Agregar queries a la pagina
    Ahora se puede filtrar la tabla por 6 campos: Titulo, director, género, año, país, y lenguaje. Además, mostramos mensajes/botones diferentes dependiendo de si hay filtros aplicados a la tabla

--- app_peliculas/views.py
+++ app_peliculas/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.views.generic.detail import DetailView
+from django.db.models.query_utils import Q
 
 from .models import Pelicula
 
@@ -18,8 +19,40 @@ class PeliculasBaseView(View):
 
 
 class PeliculasListView(PeliculasBaseView, ListView):
-    paginate_by = 10
-    ordering = ["id"]
+    paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        parametros_url = self.request.GET.dict()
+
+        filtros = Q()
+
+        if "titulo" in parametros_url:
+            filtros &= Q(titulo__istartswith=parametros_url["titulo"])
+        if "director" in parametros_url:
+            filtros &= Q(director__istartswith=parametros_url["director"])
+        if "genero" in parametros_url:
+            filtros &= Q(genero__iexact=parametros_url["genero"])
+        if "año" in parametros_url:
+            filtros &= Q(año__exact=parametros_url["año"])
+        if "pais" in parametros_url:
+            filtros &= Q(pais__iexact=parametros_url["pais"])
+        if "lenguaje" in parametros_url:
+            filtros &= Q(lenguaje__iexact=parametros_url["lenguaje"])
+
+        return queryset.filter(filtros).order_by("id")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        parametros = self.request.GET.copy()
+
+        if 'page' in parametros:
+            del parametros['page']
+
+        context['parametros'] = parametros.urlencode()
+
+        return context
 
 
 class PeliculasDetailView(PeliculasBaseView, DetailView):
