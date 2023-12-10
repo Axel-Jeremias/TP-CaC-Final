00659e1c2c44ed98beb385cb4583c6ff6c47fd6d
Axel-Jeremias <axeljeremias1@gmail.com>
2023-12-09 22:30:27 -0300

    Modificaciones para deploy

--- MasterFrame/settings.py
+++ MasterFrame/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1ksffmcuc+lz85q$9*)30j6)x)wlehmo$1z*9+=%e-_gln%5i-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["axeljeremias.pythonanywhere.com"]
 
 
 # Application definition
@@ -130,6 +130,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
